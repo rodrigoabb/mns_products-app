3099e86024201f24a1b3d445ad286a51bb85af89
Rodrigo <rodrigoabbas@gmail.com>
2021-04-26 01:12:57 -0300

    Made cards redirect to details view

--- src/components/products-grid/products-grid.component.tsx
+++ src/components/products-grid/products-grid.component.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import ProductsGridItem from './products-grid-item/products-grid-item.component';
+import { Link } from 'react-router-dom';
 
 import { IProduct } from '../../common/interfaces/product.interface';
 
@@ -12,12 +13,15 @@ interface IProductsGridProps {
 
 
 const ProductsGrid: React.FC<IProductsGridProps> = ({ products }: IProductsGridProps ) => {
+
   return (
     <ul className="products-grid">
       {
         products.map((product: IProduct) => (
           <li className="products-grid__item" key={ product.id }>
-            <ProductsGridItem product= { product }/>
+            <Link to={`product/${product.id}`} style={{ textDecoration: 'none', color: 'inherit'}}>
+              <ProductsGridItem product= { product }/>
+            </Link>
           </li>
         ))
       }
