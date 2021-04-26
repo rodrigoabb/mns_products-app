77ff0032ce6e6ddac6c745565981431de91c14b0
Rodrigo <rodrigoabbas@gmail.com>
2021-04-25 21:03:32 -0300

    Completed Product interface

--- src/common/interfaces/product.interface.ts
+++ src/common/interfaces/product.interface.ts
@@ -1,4 +1,10 @@
 export interface IProduct {
   id: string,
   name: string,
+  image_key: string,
+  price: {
+    currency_code: string,
+    current_price: number,
+    original_price: number | null,
+  }
 };
